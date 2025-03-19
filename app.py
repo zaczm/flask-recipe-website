f981a142338b1d64036873b0c120145fc9d78f4c
hamzayusufmn <122660175+hamzayusufmn@users.noreply.github.com>
2025-03-19 18:23:03 +0000

    Add banana smoothie recipe

--- app.py
+++ app.py
@@ -16,8 +16,14 @@ recipes = {
         "name": "Quick PB&J",
         "ingredients": ["bread", "peanut butter", "jelly"],
         "instructions": "1. Spread peanut butter & jelly on slices\n2. Put them together"
+    },  
+    "banana-smoothie": {
+        "name": "Banana Smoothie",
+        "ingredients": ["1 banana", "1 cup milk", "2 tbsp honey", "ice cubes"],
+        "instructions": "1. Add ingredients to blender\n2. Blend until smooth\n3. Enjoy!"
     }
 }
+
 
 
 # HTML Templates as strings
